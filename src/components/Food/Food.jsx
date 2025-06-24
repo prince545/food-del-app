56ef39a1d164a34b76dfc0ef36731eaecb4fa774
Simerjeet singh <daljeetkaur9570@gmail.com>
2025-06-24 16:12:11 +0530

    Renamed fOod.jsx to Food.jsx with correct casing

--- src/components/Food/Food.jsx
+++ src/components/Food/Food.jsx
@@ -1,34 +1,3 @@
-// import React, { useContext } from 'react';
-// import './Food.css';
-// import { StoreContext } from '../../context/StoreContext';
-// import FoodItem from '../FoodItem/FoodItem';
-
-// const Food = () => {
-//   const { food_list } = useContext(StoreContext);
-
-
-  
-
-//   return (
-//     <div className="food" id="food">
-//       <h1>Our Food</h1>
-//       <div className="food-list">
-//         {food_list.map(item => (
-//           <FoodItem 
-//             key={item.__id} // Changed to __id
-//             _id={item.__id} // Changed to __id and renamed prop to _id for FoodItem
-//             name={item.name} 
-//             price={item.price}
-//             description={item.description} 
-//             image={item.image}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Food;
 import { useContext } from 'react';
 import './Food.css';
 import { StoreContext } from '../../context/StoreContext';
@@ -66,4 +35,3 @@ const Food = ({ category }) => {
 };
 
 export default Food;
-
